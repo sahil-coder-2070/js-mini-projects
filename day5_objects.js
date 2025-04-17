e3f4afccc1d49bd511e2e31610c0ce104a5b3e1a
sahil singh <sahil171759@gmail.com>
2025-04-17 22:10:46 +0530

    Create day5_objects.js

---
+++ day5_objects.js
@@ -0,0 +1,10 @@
+// student manager list 
+let students = [
+  { name: "Ravi", age: 20, marks: 85 },
+  { name: "Anu", age: 22, marks: 90 },
+  { name: "Zoya", age: 21, marks: 75 }
+];
+
+for (let i = 0; i < students.length; i++) {
+  console.log(students[i].name + " scored " + students[i].marks);
+}
