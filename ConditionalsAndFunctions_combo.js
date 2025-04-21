b5a3e78fb93e75cf6b996638b20691710b67a89c
sahil singh <sahil171759@gmail.com>
2025-04-21 21:00:26 +0530

    Create ConditionalsAndFunctions_combo.js

---
+++ ConditionalsAndFunctions_combo.js
@@ -0,0 +1,29 @@
+// Grade calculator
+function getGrade(score) {
+  if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else {
+    return "Fail";
+  }
+}
+
+console.log(getGrade(85));  // Output: B
+
+// Traffic light Instructions
+function getGrade(score) {
+  if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else {
+    return "Fail";
+  }
+}
+
+console.log(getGrade(85));  // Output: B
