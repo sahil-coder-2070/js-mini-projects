7873450891d4dec37ad325f7b5b8a7bf1ba9f5c7
sahil singh <sahil171759@gmail.com>
2025-04-23 22:04:35 +0530

    Create day7_loops.js

---
+++ day7_loops.js
@@ -0,0 +1,17 @@
+// For Loop
+for (let i = 1; i <= 5; i++) {
+  console.log("Hello " + i);
+}
+
+// While Loop
+let i = 1;
+while (i <= 3) {
+  console.log("Bye " + i);
+  i++;
+}
+
+// For...of Loop 
+let fruits = ["apple", "banana", "mango"];
+for (let fruit of fruits) {
+  console.log(fruit);
+}
